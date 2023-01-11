8c8ea2488efb45f44b92259dbb94076cb0f8b3dd
Thibault Cheneviere <thibault.cheneviere@telecomnancy.eu>
2023-01-11 15:52:29 +0100

    [+] rework type inferer and add skeleton of AstVisitor for sem controls

---
+++ src/csem/AstVisitor.java
@@ -0,0 +1,198 @@
+package csem;
+
+import ast.*;
+
+public class AstVisitor implements ast.AstVisitor<String> {
+    @Override
+    public String visit(Program a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Expression a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Ou a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Et a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Compar a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Addition a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Soustraction a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Multiplication a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Division a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Sequence a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Negation a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(ID a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Int a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(ExpressionIdentifiant a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(AppelFonction a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(ArgFonction a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(IfThenElse a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(IfThen a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(While a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(For a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Definition a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(DeclarationType a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(DeclarationTypeClassique a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(DeclarationArrayType a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(DeclarationRecordType a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(DeclarationChamp a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(DeclarationFonction a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(DeclarationValeur a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(ChaineChr a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Nil a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(Break a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visit(InstanciationType a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
--- src/sl/TypeInferer.java
+++ src/sl/TypeInferer.java
@@ -5,13 +5,13 @@ public class TypeInferer {
         expr = expr.trim();
         expr = expr.split(" ")[0].split("\\[")[0];
 
-        if (expr.contains("-") || expr.contains("+") || expr.contains("*") || expr.contains("/")
-                || isNumeric(expr) || expr.equals("int"))
-            return new Primitive(Integer.class);
         if (table.getSymbol(expr) != null)
             return table.getSymbol(expr).getType();
         if (table.getType(expr) != null)
             return table.getType(expr);
+        if (expr.contains("-") || expr.contains("+") || expr.contains("*") || expr.contains("/")
+                || isNumeric(expr) || expr.equals("int"))
+            return new Primitive(Integer.class);
         if (expr.contains("array"))
             return new Array(inferType(table, expr.substring(7, expr.length())));
         if (expr.equals("string"))
