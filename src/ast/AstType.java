a4e6cc266fba57764b1bbc46c20a5c5da3b1b6ad
BRUNET Jules <jules.brunet@telecomnancy.eu>
2023-01-18 09:31:45 +0100

    [~] fix ast Type 2eme partie v2

--- src/ast/AstType.java
+++ src/ast/AstType.java
@@ -226,6 +226,7 @@ public class AstType extends exprBaseVisitor<Type> {
     @Override
     public Type visitListeAcces(ListeAccesContext ctx) {
         // System.out.println("Liste acces");
+        
         return null;
     }
 
