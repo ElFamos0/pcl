fb814a46b1c75420f49de4e2d46434b754a206bc
Thibault Cheneviere <thibault.cheneviere@telecomnancy.eu>
2023-04-09 22:42:55 +0200

    [+] add

--- src/asm/ASMVisitor.java
+++ src/asm/ASMVisitor.java
@@ -13,6 +13,8 @@ public class ASMVisitor implements AstVisitor<ParserRuleContext> {
     private int region;
     private int biggestRegion = 0;
 
+    private ASMWriter writer;
+
     private List<Register> registers = new ArrayList<Register>();
     private Register BasePointer = new Register("r11", 0);
     private Register StackPointer = new Register("r13", 0);
@@ -50,70 +52,6 @@ public class ASMVisitor implements AstVisitor<ParserRuleContext> {
 
     public void StepDownRegion() {
         region--;
-    }
-
-    /// Add functions
-    private void Add(Register dst, Register val1, Register val2) {
-        String instr = "add" + " " + dst.getName() + ", " + val1.getValue() + ", " + val2.getValue();
-
-        // TODO: write instruction to file
-    }
-
-    private void Add(Register dst, Register val1, int val2) {
-        String instr = "add" + " " + dst.getName() + ", " + val1.getValue() + ", #" + val2;
-
-        // TODO: write instruction to file
-    }
-
-    private void Add(Register dst, int val1, Register val2) {
-        String instr = "add" + " " + dst.getName() + ", #" + val1 + ", " + val2.getValue();
-
-        // TODO: write instruction to file
-    }
-
-    // Sub functions
-    private void Sub(Register dst, Register val1, Register val2) {
-        String instr = "sub" + " " + dst.getName() + ", " + val1.getValue() + ", " + val2.getValue();
-
-        // TODO: write instruction to file
-    }
-
-    private void Sub(Register dst, Register val1, int val2) {
-        String instr = "sub" + " " + dst.getName() + ", " + val1.getValue() + ", #" + val2;
-
-        // TODO: write instruction to file
-    }
-
-    private void Sub(Register dst, int val1, Register val2) {
-        String instr = "sub" + " " + dst.getName() + ", #" + val1 + ", " + val2.getValue();
-
-        // TODO: write instruction to file
-    }
-
-
-    /// Move functions
-    private void Mov(Register dst, Register val) {
-        String instr = "mov" + " " + dst.getName() + ", " + val.getName();
-
-        // TODO: write instruction to file
-    }
-
-    private void Mov(Register dst, int val) {
-        String instr = "mov" + " " + dst.getName() + ", #" + val;
-
-        // TODO: write instruction to file
-    }
-
-    private void Movn(Register dst, int val) {
-        String instr = "movn" + " " + dst.getName() + ", #" + val;
-
-        // TODO: write instruction to file
-    }
-
-    private void Movn(Register dst, Register val) {
-        String instr = "movn" + " " + dst.getName() + ", " + val.getName();
-
-        // TODO: write instruction to file
     }
 
     @Override
--- src/asm/ASMWriter.java
+++ src/asm/ASMWriter.java
@@ -12,4 +12,101 @@ public class ASMWriter {
         File file = new File(filename);
         writer = new FileWriter(file);
     }
+
+    public void write(String s) {
+        // Writes a string to the file
+        try {
+            writer.write(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /// Add functions
+    public void Add(Register dst, Register val1, Register val2) {
+        String instr = "add" + " " + dst.getName() + ", " + val1.getValue() + ", " + val2.getValue();
+
+        // Write instruction to file
+        write(instr);
+    }
+
+    public void Add(Register dst, Register val1, int val2) {
+        String instr = "add" + " " + dst.getName() + ", " + val1.getValue() + ", #" + val2;
+
+        // Write instruction to file
+        write(instr);
+    }
+
+    public void Add(Register dst, int val1, Register val2) {
+        String instr = "add" + " " + dst.getName() + ", #" + val1 + ", " + val2.getValue();
+
+        // Write instruction to file
+        write(instr);
+    }
+
+    public void Add(Register dst, int val1, int val2) {
+        String instr = "add" + " " + dst.getName() + ", #" + val1 + ", #" + val2;
+
+        // Write instruction to file
+        write(instr);
+    }
+
+    // Sub functions
+    public void Sub(Register dst, Register val1, Register val2) {
+        String instr = "sub" + " " + dst.getName() + ", " + val1.getValue() + ", " + val2.getValue();
+
+        // Write instruction to file
+        write(instr);
+    }
+
+    public void Sub(Register dst, Register val1, int val2) {
+        String instr = "sub" + " " + dst.getName() + ", " + val1.getValue() + ", #" + val2;
+
+        // Write instruction to file
+        write(instr);
+    }
+
+    public void Sub(Register dst, int val1, Register val2) {
+        String instr = "sub" + " " + dst.getName() + ", #" + val1 + ", " + val2.getValue();
+
+        // Write instruction to file
+        write(instr);
+    }
+
+    public void Sub(Register dst, int val1, int val2) {
+        String instr = "sub" + " " + dst.getName() + ", #" + val1 + ", #" + val2;
+
+        // Write instruction to file
+        write(instr);
+    }
+
+
+    /// Move functions
+    public void Mov(Register dst, Register val) {
+        String instr = "mov" + " " + dst.getName() + ", " + val.getName();
+
+        // Write instruction to file
+        write(instr);
+    }
+
+    public void Mov(Register dst, int val) {
+        String instr = "mov" + " " + dst.getName() + ", #" + val;
+
+        // Write instruction to file
+        write(instr);
+    }
+
+    public void Movn(Register dst, int val) {
+        String instr = "mvn" + " " + dst.getName() + ", #" + val;
+
+        // Write instruction to file
+        write(instr);
+    }
+
+    public void Movn(Register dst, Register val) {
+        String instr = "mvn" + " " + dst.getName() + ", " + val.getName();
+
+        // Write instruction to file
+        write(instr);
+    }
 }
