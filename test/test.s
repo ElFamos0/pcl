6ca103b6d2f8dc9d7efda9ae5540540af0fc7141
Tristan SMAGGHE <tristan.smagghe@telecomnancy.eu>
2023-04-10 12:14:36 +0200

    feat: cool arm emulator

---
+++ test/test.s
@@ -0,0 +1,16 @@
+.text            
+.global _start
+_start:
+    mov r0, #1
+    ldr r1, =message
+    ldr r2, =len
+    mov r7, #4
+    swi 0
+
+    mov r7, #1
+    swi 0
+
+.data
+message:
+    .asciz "je suis caca\n"
+len = .-message  
